53ea7f5272a433fc62c1781f110200df11440b8e
Mateus Dantas <mtzdantas@gmail.com>
2025-04-14 13:25:23 -0300

    feat: realiza consulta da questão 04 (ref: #1)

---
+++ tarefas/t01/tare01-q04.sql
@@ -0,0 +1,7 @@
+SELECT f.nome, f.salario, d.descricao AS departamento
+FROM funcionario f
+JOIN departamento d ON f.cod_depto = d.codigo
+WHERE f.codigo NOT IN (
+    SELECT cod_gerente FROM departamento WHERE cod_gerente IS NOT NULL
+)
+ORDER BY f.cod_depto;
